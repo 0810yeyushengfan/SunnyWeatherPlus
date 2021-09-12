7aa805220e0e79cbca0622cbb67696b023f55fb9
zyh <2025050409@henu.edu.cn>
2021-09-12 11:42:26 +0800

    增加注释4

--- app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -30,8 +30,10 @@ object Repository {
     }
 
     //刷新天气信息
+    //在仓库层我们并没有提供两个分别用于获取实时天气信息和未来天气信息的方法，而是提供了一个refreshWeather()方法用来刷新天气信息。因为对于调用方而言，需要调用两次请求才能获得其想要的所有天气数据明显是比较烦琐的行为，因此最好的做法就是在仓库层再进行一次统一的封装
     fun refreshWeather(lng:String,lat:String)= fire(Dispatchers.IO) {
-            coroutineScope {
+            coroutineScope {//由于async函数必须在协程作用域内才能调用，所以这里又使用coroutineScope函数创建了一个协程作用域
+                //只需要分别在两个async函数中发起网络请求，然后再分别调用它们的await()方法，就可以保证只有在两个网络请求都成功响应之后，才会进一步执行程序
                 val deferredRealtime=async {
                     SunnyWeatherNetwork.getRealtimeWeather(lng,lat)
                 }
@@ -40,20 +42,26 @@ object Repository {
                 }
                 val realtimeResponse=deferredRealtime.await()
                 val dailyResponse=deferredDaily.await()
+                //在同时获取到RealtimeResponse和DailyResponse之后，如果它们的响应状态都是ok
                 if(realtimeResponse.status=="ok"&&dailyResponse.status=="ok"){
+                    //那么就将Realtime和Daily对象取出并封装到一个Weather对象中
                     val weather= Weather(realtimeResponse.result.realtime,dailyResponse.result.daily)
+                    //然后使用Result.success()方法来包装这个Weather对象
                     Result.success(weather)
                 }else{
+                    //否则就使用Result.failure()方法来包装一个异常信息
                     Result.failure(RuntimeException("realtime response status is ${realtimeResponse.status}"+ "daily response status is ${dailyResponse.status}"))
                 }
             }
     }
 
-    private fun<T> fire(context:CoroutineContext,block:suspend()->Result<T>)=liveData<Result<T>>(context){
+    //一个按照liveData()函数的参数接收标准定义的一个高阶函数
+    //在liveData()函数的代码块中，我们是拥有挂起函数上下文的，可是当回调到Lambda表达式中，代码就没有挂起函数上下文了，但实际上Lambda表达式中的代码一定也是在挂起函数中运行的。为了解决这个问题，我们需要在函数类型前声明一个suspend关键字，以表示所有传入的Lambda表达式中的代码也是拥有挂起函数上下文的
+    private fun<T> fire(context:CoroutineContext,block:suspend()->Result<T>)=liveData<Result<T>>(context){//在fire()函数的内部会先调用一下liveData()函数，然后在liveData()函数的代码块中统一进行了try catch处理，并在try语句中调用传入的Lambda表达式中的代码，最终获取Lambda表达式的执行结果并调用emit()方法发射出去。
             val result=try{
                 block()
             }catch (e:Exception){
-                Result.failure<T>(e)
+                Result.failure<T>(e)//这里的泛型T为List<Weather/Place>,上面liveData的泛型为Result<List<Weather/Place>>，因为result数据被Result包装过之后才发给了liveData
             }
             emit(result)
     }
@@ -68,7 +76,7 @@ object Repository {
 //    fun searchPlaces(query:String)= liveData(Dispatchers.IO) {//我们还将liveData()函数的线程参数类型指定成了Dispatchers.IO，这样代码块中的所有代码就都运行在子线程中了。众所周知，Android是不允许在主线程中进行网络请求的，诸如读写数据库之类的本地数据操作也是不建议在主线程中进行的，因此非常有必要在仓库层进行一次线程转换
 //        val result=try{
 //            //调用SunnyWeatherNetwork的searchPlaces()函数来搜索城市数据
-//            val placeResponse=SunnyWeatherNetwork.searchPlaces(query)
+//            val placeResponse=SunnyWeatherNetwork.searchPlaces(query)//因为LiveData()函数提供了挂起函数的上下文，所以我们能够调用挂起函数
 //            if(placeResponse.status=="ok"){//判断如果服务器响应的状态是ok
 //                val place=placeResponse.places;
 //                Result.success(place)//使用Kotlin内置的Result.success()方法来包装获取的城市数据列表
--- app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -21,18 +21,19 @@ object SunnyWeatherNetwork {
     //使用ServiceCreator创建了一个PlaceService接口的动态代理对象
     private val placeService = ServiceCreator.create<PlaceService>()
 
-    //定义了一个searchPlaces()函数，并在这里调用刚刚在PlaceService接口中定义的searchPlaces()方法，以发起搜索城市数据请求
+    //定义了一个searchPlaces()函数，并在这里调用刚刚在PlaceService接口中定义的searchPlaces()方法，并利用它的返回值调用自定义的await()方法，以发起搜索城市数据请求
     suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
 
     //当外部调用SunnyWeatherNetwork的searchPlaces()函数时，Retrofit就会立即发起网络请求，同时当前的协程也会被阻塞住。直到服务器响应我们的请求之后，await()函数会将解析出来的数据模型对象取出并返回，同时恢复当前协程的执行，searchPlaces()函数在得到await()函数的返回值后会将该数据再返回到上一层
-    private suspend fun <T> Call<T>.await(): T {
-        return suspendCoroutine { continuation ->
+    private suspend fun <T> Call<T>.await(): T {//首先await()函数仍然是一个挂起函数，然后我们给它声明了一个泛型T，并将await()函数定义成了Call<T>的扩展函数，这样所有返回值是Call类型的Retrofit网络请求接口就都可以直接调用await()函数了
+        return suspendCoroutine { continuation ->//await()函数中使用了suspendCoroutine函数来挂起当前协程
             //调用enqueue()方法，Retrofit就会根据注解中配置的服务器接口地址去进行网络请求，服务器响应的数据会回调到enqueue()方法中传入的Callback实现里面,需要注意的是，当发起请求的时候，Retrofit会自动在内部开启子线程，当数据回调到Callback中之后，Retrofit又会自动切换回主线程，整个操作过程中我们都不用考虑线程切换问题
-            enqueue(object : Callback<T> {
+            enqueue(object : Callback<T> {//由于扩展函数的原因，我们现在拥有了Call对象的上下文，那么这里就可以直接调用enqueue()方法让Retrofit发起网络请求
                 //网络请求成功之后回调此方法,并将结果放入response中
                 override fun onResponse(call: Call<T>, response: Response<T>) {
                     //在Callback的onResponse()方法中，调用response.body()方法将会得到Retrofit解析后的对象，也就是PlaceResponse类型的数据
                     val body = response.body()
+                //，在onResponse()回调当中，我们调用body()方法解析出来的对象是可能为空的。如果为空的话，要手动抛出一个异常
                     if (body != null) continuation.resume(body)
                     else continuation.resumeWithException(RuntimeException("response body is null"))
                 }
